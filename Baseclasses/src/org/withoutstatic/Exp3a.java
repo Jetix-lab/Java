e4f86857c8c0bed92e72f425778ff463ef2e686c
Jetix-lab <ganesh24124@gmail.com>
2024-07-16 11:25:24 +0530

    Delete Baseclasses directory

--- Baseclasses/src/org/withoutstatic/Exp3a.java
+++
@@ -1,7 +0,0 @@
-package org.withoutstatic;
-
-//access instance variable into non-static method using extends
-public class Exp3a {
-	int id =100;
-
-}
--- Baseclasses/src/org/withstatic/Exp17a.java
+++
@@ -1,9 +0,0 @@
-package org.withstatic;
-
-public class Exp17a {
-	static int a= 10;
-	public static void m1() {
-		System.out.println(a);
-	}
-
-}
